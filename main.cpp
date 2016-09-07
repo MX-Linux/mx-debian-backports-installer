e049437ca6e87616c19fdeeb096ce74009a1dc77
Dolphin Oracle <dolphinoracle@gmail.com>
2016-09-06 20:18:55 -0400

    initial release

---
+++ main.cpp
@@ -0,0 +1,61 @@
+/**********************************************************************
+ *  main.cpp
+ **********************************************************************
+ * Copyright (C) 2016 MX Authors
+ *
+ * Authors: Adrian
+ *          dolphin_oracle
+ *          MX & MEPIS Community <http://forum.mepiscommunity.org>
+ *
+ * This file is part of mx-debian-backports-installer
+ *
+ * mx-debian-backports-installer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mx-debian-backports-installer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mx-debian-backports-installer.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+
+#include "mainwindow.h"
+#include <QApplication>
+#include <unistd.h>
+#include <QApplication>
+#include <QTranslator>
+#include <QLocale>
+#include <QIcon>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator qtTran;
+    qtTran.load(QString("qt_") + QLocale::system().name());
+    a.installTranslator(&qtTran);
+
+    QTranslator appTran;
+    appTran.load(QString("mx-debian-backports-installer_") + QLocale::system().name(), "/usr/share/mx-debian-backports-installer/locale");
+    a.installTranslator(&appTran);
+
+    if (getuid() == 0) {
+        MainWindow w;
+        w.show();
+
+        return a.exec();
+
+    } else {
+        QApplication::beep();
+        QMessageBox::critical(0, QString::null,
+                              QApplication::tr("You must run this program as root."));
+        return 1;
+    }
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,83 @@
+/**********************************************************************
+ *  mainwindow.h
+ **********************************************************************
+ * Copyright (C) 2016 MX Authors
+ *
+ * Authors: dolphin_oracle
+ *          Adrian
+ *          MX & MEPIS Community <http://forum.mepiscommunity.org>
+ *
+ * This file is part of mx-debian-backports-installer
+ *
+ * mx-debian-backports-installer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mx-debian-backports-installer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mx-debian-backports-installer.  If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QTreeWidgetItem>
+#include <QTimer>
+#include <QProgressDialog>
+#include <QProgressBar>
+#include <QLineEdit>
+
+namespace Ui {
+class MainWindow;
+}
+
+// struct for outputing both the exit code and the strings when running a command
+struct Output {
+    int exit_code;
+    QString str;
+};
+
+class MainWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    QString version;
+    QStringList changeset;
+    QStringList readMXlist();
+
+    void displayMXlist(QStringList mxlist);    
+    void start();
+    void startProgressBar();
+    void stopProgressBar();
+    QString getVersion(QString name);
+    Output runCmd(QString cmd);
+
+private slots:
+    void closeSearch();
+    void search();
+    void findPackage();
+    void procTime();
+    void on_buttonCancel_clicked();
+    void on_buttonInstall_clicked();
+    void on_buttonAbout_clicked();
+    void on_treeWidget_itemClicked(QTreeWidgetItem *item);
+    void on_buttonHelp_clicked();
+
+private:            
+    QProgressDialog *progress;
+    QTimer *timer;
+    QLineEdit *searchBox;
+    Ui::MainWindow *ui;
+    void keyPressEvent(QKeyEvent *event);
+};
+
+#endif // MAINWINDOW_H
