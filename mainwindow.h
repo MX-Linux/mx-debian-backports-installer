a5516e7e61cfee34455d858426ae1e8fbd375609
Dolphin Oracle <dolphinoracle@gmail.com>
2016-09-08 20:27:14 -0400

    updated with corrected source

--- mainwindow.h
+++ mainwindow.h
@@ -54,11 +54,10 @@ public:
     QStringList changeset;
     QStringList readMXlist();
 
-    void displayMXlist(QStringList mxlist);
+    void displayMXlist(QStringList mxlist);    
     void start();
     void startProgressBar();
     void stopProgressBar();
-
     QString getVersion(QString name);
     Output runCmd(QString cmd);
 
@@ -73,9 +72,10 @@ private slots:
     void on_treeWidget_itemClicked(QTreeWidgetItem *item);
     void on_buttonHelp_clicked();
 
-private:
+private:            
     QProgressDialog *progress;
     QTimer *timer;
+    QLineEdit *searchBox;
     Ui::MainWindow *ui;
     void keyPressEvent(QKeyEvent *event);
 };
