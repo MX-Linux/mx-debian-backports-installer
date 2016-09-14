4d80a35f639c83ab416867531de1cedef93be334
Adrian <adrian@mxlinux.org>
2016-09-14 12:44:13 -0400

    minor edit

--- lockfile.cpp
+++ lockfile.cpp
@@ -9,7 +9,7 @@
 *
 * This file is part of mx-debian-backports-installer
 *
-* mx-test-repo-installer is free software: you can redistribute it and/or modify
+* This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
